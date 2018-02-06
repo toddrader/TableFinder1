b2b7305f13347ceaa70b10a498a0c1a3813758f9
Todd Rader <todd@toddrader.com>
2018-02-06 12:53:40 -0500

    all

--- script.js
+++ script.js
@@ -5,26 +5,15 @@ $(".reservation").hide();
     $(this).css("background", "#ADAAAA");
     $(this).removeClass("available").addClass("reserved");
     tableSelected = $(this).attr("class")[0];
+    $(".tableNumberDisplay").text("Table Number: " + tableSelected);
     $(".reservation").show(1500);
   });
   $(".xOut").click(function() {
-    console.log(tableSelected)
-    // $("class=["table"]class=["5"]").css("background", "#EEE");
-    // $("class=["table"]class=["5"]").removeClass("reserved").addClass("available");
-    $(".reserved").css("background", "#EEE");
-    $(".reserved").removeClass("reserved").addClass("available");
-    // $(".reserved:nth-of-type(tableSelected-1)").css("background", "#EEE");
-    // $(".reserved:nth-of-type(tableSelected-1)").removeClass("reserved").addClass("available");
+    $('.'+tableSelected).css("background", "#EEE");
+    $('.'+tableSelected).removeClass("reserved").addClass("available");
     $(".reservation").hide(1500);
   });
   $(".xOut, button").click(function() {
     $(".reservation").hide(1500);
   });
-// "unclicking the table"
-  // $(".1.reserved").click(function() {
-  //   $(this).css("background", "#EEE");
-  //   $(this).removeClass("reserved").addClass("available");
-  //   var classes = $(this).attr("class");
-  // console.log(classes);
-  // });
 });
